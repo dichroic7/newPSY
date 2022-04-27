6ff9e06f2560ef3afc1b388b286677deff29dc8a
dichroic7 <104331454+dichroic7@users.noreply.github.com>
2022-04-28 01:33:26 +0900

    ~D3

---
+++ java/src/day3/Ex12_for3.java
@@ -0,0 +1,18 @@
+package day3;
+
+public class Ex12_for3 {
+
+	public static void main(String[] args) {
+		// 1부터 10사이의 짝수를 출력하는 코드 작성
+		int i;
+		for(i = 1; i <= 5; i++)
+		{
+			System.out.println(i*2);
+		}
+		for(i=2; i<=10; i+=2)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
---
+++ java/src/day3/Ex14_for5.java
@@ -0,0 +1,18 @@
+package day3;
+
+public class Ex14_for5 {
+
+	public static void main(String[] args) 
+	{
+		// 정수 i의 약수를 출력하는 코드 작성
+		int i = 12;
+		for(int j=1; j<=i; j++)
+		{
+			if(i % j == 0)
+			{
+				System.out.println(j);
+			}
+		}
+	
+	}
+}
