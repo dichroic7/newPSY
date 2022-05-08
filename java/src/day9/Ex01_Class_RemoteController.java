41b916aea6e145863018ba09d207a44fe128c0a1
dichroic7 <104331454+dichroic7@users.noreply.github.com>
2022-05-09 02:12:13 +0900

    Until Week2

---
+++ java/src/day9/Ex01_Class_RemoteController.java
@@ -0,0 +1,14 @@
+package day9;
+
+public class Ex01_Class_RemoteController {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
+
+/* Tv 리모컨 클래스를 생성하고, 객체를 생성 후 기능들을 테스트해보세요. */
+class RemoteController{
+	
+}
